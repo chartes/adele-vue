7f47c5715afca6b80ee55322e314206861425c75
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2019-11-22 16:08:12 +0100

    add leaflet deps

--- src/main.js
+++ src/main.js
@@ -8,6 +8,19 @@ import './assets/sass/main.scss'
 
 Vue.config.productionTip = false;
 
+import { Icon }  from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+
+// this part resolve an issue where the markers would not appear
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 new Vue({
   el: '#app',
   store,
