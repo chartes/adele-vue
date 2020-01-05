4b597f2da67fc1fd54108abac0c015c7d59f98f6
Denis Chiron <chiron.denis@gmail.com>
2020-01-05 18:57:44 +0100

    - Ajout des images de la charte - Debut de structure accueil : 3 colonnes

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="section">
-    <div class="container">
-      Landing page
+  <div class="container landing-page">
+    <div class="columns">
+      <div class="column">
+        Dossiers thématiques
+      </div>
+      <div class="column is-half">
+        <h1>Bienvenue</h1>
+        <p>
+          Développée par l’École nationale des chartes avec le soutien de Scripta et du DIM Ile-de-France, l’application ADELE entend
+          proposer à un public d’étudiants et de chercheurs un manuel de diplomatique progressivement construit autour du déchiffrement
+          interactif, de l’annotation, si besoin de la traduction et enfin de l’analyse diplomatique d’actes et de
+          documents de gestion. Appuyée sur un outil de mise au point collaborative des dossiers, en classe ou par des chercheurs
+          associés, elle est dès l’origine conçue pour dilater le champ de vision de son noyau primitif (actes médiévaux français) dans
+          l’espace, le temps, le type.
+        </p>
+      </div>
+      <div class="column">
+        Derniers documents
+      </div>
     </div>
   </div>
 </template>
