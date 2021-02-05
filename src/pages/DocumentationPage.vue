f3a3fccd4094c91af5ada50e4694eb96caf908c7
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2021-02-05 11:15:32 +0100

    add indentation to the documentation page

--- src/pages/DocumentationPage.vue
+++ src/pages/DocumentationPage.vue
@@ -184,15 +184,13 @@
             v-if="$attrs.section === 'credits' || $attrs.section === null"
           >
             <h1>Crédits</h1>
-            <p>
-              Projet soutenu par <a
-                href="https://scripta.psl.eu/"
-                target="_blank"
-              >Scripta</a> et par le DIM STCN, placé sous le patronage de la <a
-                href="https://cidipl.org/"
-                target="_blank"
-              >Commission internationale de diplomatique</a>.
-            </p>
+            Projet soutenu par <a
+              href="https://scripta.psl.eu/"
+              target="_blank"
+            >Scripta</a> et par le DIM STCN, placé sous le patronage de la <a
+              href="https://cidipl.org/"
+              target="_blank"
+            >Commission internationale de diplomatique</a>.
             <ul>
               <li>Conception scientifique : Olivier Guyotjeannin</li>
               <li>Conception informatique : École des chartes, missions projets numériques</li>
@@ -262,10 +260,10 @@
               A – Actes, auteurs
             </h2>
             <p>
-              Il est nécessaire de faire la distinction entre l'auteur de l'acte écrit et l'auteur de l'action juridique. <br>
+              Il est nécessaire de faire la distinction entre l'auteur de l'acte écrit et l'auteur de l'action juridique.
               L'auteur de l'acte écrit est <i>l'auctor</i>, celui qui garantit et protège l'acte. Son nom est présent en début d'acte, dans la suscription, et est normalement rappelé par le sceau ou dans les validations finales.
+              Les termes et le découpage sont modelés au plus près possible sur ceux de la base <a href="http://elec.enc.sorbonne.fr/bede/">Bède</a>.
             </p>
-            <p>Les termes et le découpage sont modelés au plus près possible sur ceux de la base <a href="http://elec.enc.sorbonne.fr/bede/">Bède</a>.</p>
             <dl>
               <dt id="ac-pape">
                 Pape et entourage
@@ -1473,8 +1471,6 @@
               <dd>
                 <p>
                   Le commentaire diplomatique est le principal commentaire à fournir. Il contient notamment des éléments relatifs aux caractères externes, aux dimensions du manuscrit, à son écriture. Ce commentaire permet de replacer le document parmi les autres documents de la même typologie. Ainsi, s'il s'agit un mandement royal, on pourra le comparer aux autres mandements de la même époque, dans les formules utilisés, dans ses dimensions et son écriture.
-                </p>
-                <p>
                   Ce commentaire peut également servir de commentaire général sur le manuscrit, en l'absence d'éléments suffisant pour créer un autre commentaire.
                 </p>
                 <p>
@@ -1485,40 +1481,52 @@
                 Commentaire historique
               </dt>
               <dd>
-                Le commentaire historique explique le contexte de rédaction de l'acte. Il peut permettre ou non de faire une critique de l'exposé indiqué en début d'acte, et motivant la décision qui a été prise. <br>
-                Il peut faire le rappel des précédents actes relatifs au même sujet.<br>
-                Egalement, il peut faire un rappel de l'historique de document de la même typologie, si cela est pertinent. <br>
-                Par exemple, un document tel qu'un censier peut faire l'objet d'un commentaire historique, en l'étudiant par rapport aux autres censiers plus anciens de la même province.
+                <p>
+                  Le commentaire historique explique le contexte de rédaction de l'acte. Il peut permettre ou non de faire une critique de l'exposé indiqué en début d'acte, et motivant la décision qui a été prise. <br>
+                  Il peut faire le rappel des précédents actes relatifs au même sujet.<br>
+                  Egalement, il peut faire un rappel de l'historique de document de la même typologie, si cela est pertinent. <br>
+                  Par exemple, un document tel qu'un censier peut faire l'objet d'un commentaire historique, en l'étudiant par rapport aux autres censiers plus anciens de la même province.
+                </p>
               </dd>
               <dt id="co-juridique">
                 Commentaire juridique
               </dt>
               <dd>
-                Le commentaire juridique s'appuie sur les caractères externes qui se trouvent en le document. Par exemple, l'étude peut porter sur les éléments de validation de l'acte, qui donnent la force juridique au document. <br>
-                Aussi, le commentaire juridique peut s'appuyer sur le contenu textuel du document en question.
+                <p>
+                  Le commentaire juridique s'appuie sur les caractères externes qui se trouvent en le document. Par exemple, l'étude peut porter sur les éléments de validation de l'acte, qui donnent la force juridique au document.
+                  Aussi, le commentaire juridique peut s'appuyer sur le contenu textuel du document en question.
+                </p>
               </dd>
               <dt id="co-philolo">
                 Commentaire philologique
               </dt>
               <dd>
-                Le commentaire philologique étudie la langue et les expressions utilisées. Celui-ci peut reprendre et étudier de façon plus précise certains passages du contenu textuel du document, en faisant par exemple le repérage d'éléments d'accentuation, parmi les termes utilisés, le rythme du texte (en repérant par exemple le <i>cursus velox</i>).
+                <p>
+                  Le commentaire philologique étudie la langue et les expressions utilisées. Celui-ci peut reprendre et étudier de façon plus précise certains passages du contenu textuel du document, en faisant par exemple le repérage d'éléments d'accentuation, parmi les termes utilisés, le rythme du texte (en repérant par exemple le <i>cursus velox</i>).
+                </p>
               </dd>
               <dt id="co-paleo">
                 Commentaire paléographique
               </dt>
               <dd>
-                Ce commentaire s'appuie essentiellement sur l'écriture du document. On peut y décrire les longueur des hastes et des hampes, des caractères se distinguant plus nettement du corps du texte. <br>
-                Aussi on pourra également y commenter les abréviations et les signes de ponctuation utilisés ou non dans le texte. Les formes et les lettres qui sont abrégés pourront être décrites, pour montrer une certaine singularité ou au contraire, une homogénéité des caractères.
+                <p>
+                  Ce commentaire s'appuie essentiellement sur l'écriture du document. On peut y décrire les longueur des hastes et des hampes, des caractères se distinguant plus nettement du corps du texte.
+                  Aussi on pourra également y commenter les abréviations et les signes de ponctuation utilisés ou non dans le texte. Les formes et les lettres qui sont abrégés pourront être décrites, pour montrer une certaine singularité ou au contraire, une homogénéité des caractères.
+                </p>
               </dd>
               <dt id="co-sigillo">
                 Commentaire sigillographique
               </dt>
               <dd>
-                Le commentaire sigillographique porte sur l'étude du sceau.
-                Il est autant intéressant d'étudier l'iconographie, les éléments représentés dans le sceau (le vêtement, l'armement, la symbolique), la couleur et la matière du sceau. <br>
-                Aussi, le sceau peut contenir des éléments textuels, pouvant reprendre la suscription en début d'acte. <br>
-                Autre élément pouvant être décrit dans ce commentaire : la façon dont est scellé l'acte. Il peut parfois être fait sur simple queue, sur double queue, et peuvent être représentatifs ou non d'un type d'acte. <br>
-                La base de données <a href="www.sigilla.org">Sigilla</a> peut être riche en informations sur les sceaux, et donne des éléments utiles à leur description.
+                <p>
+                  Le commentaire sigillographique porte sur l'étude du sceau.
+                  Il est autant intéressant d'étudier l'iconographie, les éléments représentés dans le sceau (le vêtement, l'armement, la symbolique), la couleur et la matière du sceau.
+                  Aussi, le sceau peut contenir des éléments textuels, pouvant reprendre la suscription en début d'acte.
+                </p>
+                <p>
+                  Autre élément pouvant être décrit dans ce commentaire : la façon dont est scellé l'acte. Il peut parfois être fait sur simple queue, sur double queue, et peuvent être représentatifs ou non d'un type d'acte.
+                  La base de données <a href="www.sigilla.org">Sigilla</a> peut être riche en informations sur les sceaux, et donne des éléments utiles à leur description.
+                </p>
               </dd>
             </dl>
           </div>
@@ -1725,8 +1733,6 @@
             </p>
             <p>
               Les listes de dossiers (liste générale, listes particulières obtenues après tri) sont classées par date, plus exactement par année, autant que possible ramenée au style du 1<sup>er</sup> janvier.
-            </p>
-            <p>
               Les actes/documents de datation incertaine sont classés à la date la plus récente possible.
             </p>
           </div>
