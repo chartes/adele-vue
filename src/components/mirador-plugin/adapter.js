996950f863f88e10226aacdc8bcf605d11c43cf5
François Ferry <francois.ferry@logilab.fr>
2021-03-02 11:44:29 +0100

    send svgSelector to the backend at annotation creation

--- src/components/mirador-plugin/adapter.js
+++ src/components/mirador-plugin/adapter.js
@@ -10,6 +10,8 @@ export default class AdeleStorageAdapter {
   
     /** */
     async create(annotation) {
+      let fragment = null;
+      let svg = null;
       const emptyAnnoPage = {
         id: 0,
         items: [],
@@ -17,11 +19,14 @@ export default class AdeleStorageAdapter {
       };
       const annotationPage = emptyAnnoPage;
       annotationPage.items.push(annotation);
-      const matchXYWH = annotation.target.selector[0].value.match(/xywh=(.*)$/);
-      if (!matchXYWH) {
-          return emptyAnnoPage;
+      const fragmentSelector = annotation.target.selector.find(({type}) => type === "FragmentSelector");
+      if (fragmentSelector) {
+        fragment = fragmentSelector.value.match(/xywh=(.*)$/)[1];
       }
-      const [x, y, w, h] = matchXYWH[1].split(',').map(str => parseInt(str, 10));
+      const svgSelector = annotation.target.selector.find(({type}) => type === "SvgSelector");
+      if (svgSelector) {
+        svg = svgSelector.value;
+      }
       http.post(`iiif/${this.documentId}/annotations`,
         {
             manifest_url: this.manifestUrl,
@@ -29,7 +34,8 @@ export default class AdeleStorageAdapter {
             // In case there are multiple images on a canvas, optionnal, default is 0
             img_idx: 0,
             zone_type_id: 2,
-            coords: `${x},${y},${w},${h}`,
+            fragment: fragment,
+            svg: svg,
     
             // in case of a COMMENTING motivation, the text content is embedded within the annotation,
             // optionnal, default is none
@@ -58,6 +64,7 @@ export default class AdeleStorageAdapter {
 
     /** */
     async all() {
-        return http.get(`iiif/${this.documentId}/list/commenting-0`).then((response) => response.data);
+      return JSON.parse(localStorage.getItem(this.documentId));
+      //return http.get(`iiif/${this.documentId}/list/commenting-0`).then((response) => response.data);
     }
 }
